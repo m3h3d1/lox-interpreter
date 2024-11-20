5478a44a5a1a083e3d9a994a59485e22380bc2f0
Mehedi Hasan (Parvez) <hasanmehedi185@gmail.com>
2024-11-20 13:35:07 +0600

    while loop

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -47,6 +47,7 @@ public class Parser {
     private Stmt statement() {
         if(match(TokenType.IF)) return ifStatement();
         if(match(TokenType.PRINT)) return printStatement();
+        if(match(TokenType.WHILE)) return whileStatement();
         if(match(TokenType.LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
     }
@@ -80,6 +81,14 @@ public class Parser {
 
         consume(TokenType.SEMICOLON, "Expect ';' after variable declaration.");
         return new Stmt.Var(name, initializer);
+    }
+
+    private Stmt whileStatement() {
+        consume(TokenType.LEFT_PAREN, "Expect '(' after while.");
+        Expr condition = expression();
+        consume(TokenType.RIGHT_PAREN, "Expect ')' after condition.");
+        Stmt body = statement();
+        return new Stmt.While(condition, body);
     }
 
     private Stmt expressionStatement() {
